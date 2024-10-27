451b970b14ee6bf1b544d92b7864ac1b69f1ebff
Gennadiy <renardes@gmail.com>
2024-10-27 21:09:15 +0500

    операторы in, not in, is, is not

---
+++ base/operators3.py
@@ -0,0 +1,46 @@
+# >>> 'bc' in 'abcd'
+# True
+# >>>
+# >>> 'a' in 'abcd'
+# True
+# >>>
+# >>> 'bd' in 'abcd'
+# False
+
+
+# >>> 3 in [1, 2, 3, 4, 5]
+# True
+# >>>
+# >>> [3, 4] in [1, 2, 3, 4, 5]
+# False
+# >>>
+# >>>
+# >>> [3, 4] in [1, 2, [3, 4], 5, 6]
+# True
+
+
+nums1 = [1, 2, 5, 6]
+nums2 = [1, 2, 5, 6]
+
+# >>> nums1 == nums2
+# True
+# >>>
+# >>> nums1 is nums2
+# False
+# >>>
+# >>> id(nums1) == id(nums2)
+# False
+# >>>
+# >>> id(nums1)
+# 2208769823168
+# >>>
+# >>> id(nums2)
+# 2208769824896
+
+
+# >>> 3 not in nums1
+# True
+
+# >>> nums1 is not nums2
+# True
+
