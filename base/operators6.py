0fe41339d6d423e94e0ec9bd37d6812509c91fb0
Gennadiy <renardes@gmail.com>
2024-11-03 10:56:15 +0500

    взаимозаменяемость цепочки or с оператором in

---
+++ base/operators6.py
@@ -0,0 +1,10 @@
+word = input('слово: ')
+
+if word == 'abc' or word == 'def' or word == 'ghi' or word == 'jkl':
+    print('wow')
+
+# то же самое
+
+if word in ['abc', 'def', 'ghi', 'jkl']:
+    print('wow')
+
