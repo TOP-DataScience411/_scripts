aa3973210dc9f55d45a4f4fe77ddff874bfec657
Gennadiy <renardes@gmail.com>
2024-11-03 10:56:15 +0500

    многострочный способ записи составных выражений

---
+++ base/operators7.py
@@ -0,0 +1,19 @@
+num = int(input('число = '))
+
+if num == 1 or num == 3 or num == 5 or num == 7 or num == 11 or num == 13 or num == 17 or num == 19 or num == 23 or num == 29:
+    ...
+
+if (
+       num == 1 
+    or num == 3 
+    or num == 5 
+    or num == 7 
+    or num == 11 
+    or num == 13 
+    or num == 17 
+    or num == 19 
+    or num == 23 
+    or num == 29 
+):
+    ...
+
