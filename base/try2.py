c87d4e7c021ad4c5761ea89cb519e784406176c5
Gennadiy <renardes@gmail.com>
2024-11-03 10:56:14 +0500

    общий вид и варианты использования инструкции try

---
+++ base/try2.py
@@ -0,0 +1,40 @@
+# try:
+#     блок_с_потенциальной_ошибкой
+# 
+# except ValueError:
+#     блок_при_перехвате_ValueError
+# 
+# except IndexError as exception_object:
+#     блок_при_перехвате_IndexError_с_объектом_исключения
+# 
+# except (TypeError, AttributeError):
+#     блок_при_перехвате_TypeError_или_AttributeError
+# 
+# ...
+# 
+# except:
+#     блок_при_перехвате_любого_другого_исключения
+# 
+# else:
+#     блок_при_отсутствии_исключения
+# 
+# finally:
+#     блок_выполняющийся_всегда
+
+
+# try - except
+# try - except - else
+# try - except - finally
+# try - except - else - finally
+# try - finally
+
+
+try:
+    num = int(input('введите число: '))
+
+except ValueError:
+    print('вводите только цифры')
+
+else:
+    print(num)
+
