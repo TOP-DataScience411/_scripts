0e48dd7361ed7a149bd3c39a8abb30a3d2147212
Gennadiy <renardes@gmail.com>
2024-11-03 10:56:15 +0500

    изменение приоритетов логических операторов

---
+++ base/operators5.py
@@ -0,0 +1,36 @@
+# >>> False or False and False
+# False
+# >>> False or False and True
+# False
+# >>> False or True and False
+# False
+# >>> False or True and True
+# True
+# >>>
+# >>> True or False and False
+# True
+# >>> True or False and True
+# True
+# >>> True or True and False
+# True
+# >>> True or True and True
+# True
+# >>>
+# >>> (False or False) and False
+# False
+# >>> (False or False) and True
+# False
+# >>> (False or True) and False
+# False
+# >>> (False or True) and True
+# True
+# >>>
+# >>> (True or False) and False
+# False
+# >>> (True or False) and True
+# True
+# >>> (True or True) and False
+# False
+# >>> (True or True) and True
+# True
+
