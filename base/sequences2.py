40f88b3144551bac3492c3260593167e93b4976e
Gennadiy <renardes@gmail.com>
2024-11-12 00:47:24 +0500

    мутируемые и немутируемые последовательности

---
+++ base/sequences2.py
@@ -0,0 +1,45 @@
+# >>> nums = [1, 2, 4, 7]
+# >>>
+# >>> id(nums)
+# 2552248127936
+# >>>
+# >>> nums.append(13)
+# >>> nums
+# [1, 2, 4, 7, 13]
+# >>>
+# >>> id(nums)
+# 2552248127936
+# >>>
+# >>> nums += [15]
+# >>> nums
+# [1, 2, 4, 7, 13, 15]
+# >>>
+# >>> id(nums)
+# 2552248127936
+
+
+# >>> letters = ('a', 'i', 'u', 'o')
+# >>>
+# >>> id(letters)
+# 2552250139488
+# >>>
+# >>> letters += ('e',)
+# >>> letters
+# ('a', 'i', 'u', 'o', 'e')
+# >>>
+# >>> id(letters)
+# 2552250033200
+
+
+# >>> txt = 'mutable and '
+# >>>
+# >>> id(txt)
+# 2552250059952
+# >>>
+# >>> txt += 'immutable sequences'
+# >>> txt
+# 'mutable and immutable sequences'
+# >>>
+# >>> id(txt)
+# 2552250139712
+
