24abe40628d3848cd59fee77fc4a4c59f66b8325
Gennadiy <renardes@gmail.com>
2024-11-12 00:47:21 +0500

    неявная распаковка

---
+++ base/unpack.py
@@ -0,0 +1,37 @@
+# >>> t = (10, 20)
+# >>> type(t)
+# <class 'tuple'>
+# >>>
+# >>> t[0]
+# 10
+# >>> t[1]
+# 20
+# >>>
+# >>> var1, var2 = t
+# >>> var1
+# 10
+# >>> var2
+# 20
+
+
+# >>> var1, var2 = (22, 44, 66)
+# ValueError: too many values to unpack (expected 2)
+# >>>
+# >>>
+# >>> var1, var2, var3 = (22, 44, 66)
+# >>> var1
+# 22
+# >>> var2
+# 44
+# >>> var3
+# 66
+
+
+# >>> var1, var2, var3 = 101, 102, 105
+# >>> var1
+# 101
+# >>> var2
+# 102
+# >>> var3
+# 105
+
