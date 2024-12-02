fdfa80378df8d42f776b130ede09215682b89a73
Gennadiy <renardes@gmail.com>
2024-12-02 23:04:33 +0500

    решение проблемы закольцованного импорта

--- namespaces/cycled1.py
+++ namespaces/cycled1.py
@@ -1,4 +1,13 @@
+# import cycled2
+# var = 'просто переменная'
+# print(cycled2.var)
+
+# приведёт к: 
+# AttributeError: partially initialized module 'cycled2' has no attribute 'var' (most likely due to a circular import)
+
+
+import cycled_data
 import cycled2
-var = 'просто переменная'
-print(cycled2.var)
+
+print(cycled_data.var2)
 
--- namespaces/cycled2.py
+++ namespaces/cycled2.py
@@ -1,4 +1,14 @@
-import cycled1
-print(cycled1.var)
-var = 200
+# import cycled1
+# print(cycled1.var)
+# var = 200
+
+# приведёт к: 
+# AttributeError: partially initialized module 'cycled2' has no attribute 'var' (most likely due to a circular import)
+
+
+import cycled_data
+# механизм отложенного импорта приведёт к повторному выполнению кода модуля cycled1
+# import cycled1
+
+print(cycled_data.var1)
 
