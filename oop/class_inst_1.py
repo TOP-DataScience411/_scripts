031a7a0f7e4d993870f590123607db0521999189
Gennadiy <renardes@gmail.com>
2024-12-14 19:36:12 +0500

    внутренние пространства имён класса и экземпляра

--- oop/class_inst_1.py
+++ oop/class_inst_1.py
@@ -46,3 +46,16 @@ print(*dinner_table.objects)
 # keyboard mouse
 # dishes
 
+
+# внутреннее пространство имён экземпляра
+# >>> computer_table.__dict__
+# {'legs': 6, 'height': 650, 'length': 1550, 'width': 800, 'objects': ['keyboard', 'mouse']}
+
+# внутреннее пространство имён объекта класса
+# >>> Table.__dict__
+# mappingproxy({'__module__': '__main__', '__init__': <function Table.__init__ at 0x0000019DD98339C0>, 'put_on_table': <function Table.put_on_table at 0x0000019DD9833A60>, '__dict__': <attribute '__dict__' of 'Table' objects>, '__weakref__': <attribute '__weakref__' of 'Table' objects>, '__doc__': None})
+
+# полностью расширенная область видимости экземпляра
+# >>> dir(computer_table)
+# ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'height', 'legs', 'length', 'objects', 'put_on_table', 'width']
+
