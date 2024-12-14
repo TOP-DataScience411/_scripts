4ebcd3cfb1b9112be391799a87405ee888c30804
Gennadiy <renardes@gmail.com>
2024-12-14 19:32:24 +0500

    способы указания путей

---
+++ files/path_literals.py
@@ -0,0 +1,25 @@
+# абсолютные пути
+
+# пути в ОС windows
+'c:\\windows\\system32'
+r'c:\windows\system32\notepad.exe'
+
+# пути в unix-подобных ОС
+'/home/username/scripts/411/files'
+'/etc/hosts'
+
+
+# текущий рабочий каталог (cwd — current working directory)
+r'd:\G-Doc\TOP Academy\Data Science\411\scripts'
+
+# >>> from pathlib import Path
+# >>>
+# >>> Path.cwd()
+# WindowsPath('D:/G-Doc/TOP Academy/Data Science/411/scripts')
+
+
+# относительные пути
+r'files\test_file'
+r'namespaces\package\sub_package_1\utils.py'
+r'git1.txt'
+
