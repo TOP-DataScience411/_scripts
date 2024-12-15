4cd16648914ad6b1b9ff5e326e5a8ec5f635b279
Gennadiy <renardes@gmail.com>
2024-12-15 16:25:42 +0500

    декорирование функции
    как на самом деле осуществляется декорирование

--- functional/decorators.py
+++ functional/decorators.py
@@ -10,3 +10,34 @@ def decorator(function_object):
     print('конец вызова декоратора')
     return wrapper_function
 
+
+def test():
+    print('вызов декорируемой функции')
+
+
+test()
+
+# test = decorator(test)
+# test()
+
+# >>> test
+# <function test at 0x000001B4F68739C0>
+# >>>
+# >>> test.__name__
+# 'test'
+# >>>
+# >>> test = decorator(test)
+# начало вызова декоратора
+# конец вызова декоратора
+# >>>
+# >>> test
+# <function decorator.<locals>.wrapper_function at 0x000001B4F6873B00>
+# >>>
+# >>> test.__name__
+# 'wrapper_function'
+# >>>
+# >>> test()
+# начало вызова обёртки
+# вызов декорируемой функции
+# конец вызова обёртки
+
