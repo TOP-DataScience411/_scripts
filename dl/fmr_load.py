3653147064a9fefba45ba7640e720e0c052b7259
Gennadiy <renardes@gmail.com>
2025-07-06 22:25:02 +0500

    изменение функций загрузки и масштабирования изображений

--- dl/fmr_load.py
+++ dl/fmr_load.py
@@ -1,6 +1,6 @@
 from keras.utils import to_categorical
 from numpy import array, ndarray, savez_compressed
-from PIL import Image
+from PIL import Image, ImageFile
 from PIL.Image import Resampling
 
 from pathlib import Path
@@ -16,23 +16,33 @@ data_dir = script_dir / 'fmr'
 #   1 — Mercedes
 #   2 — Renault
 
-def load_images(
+def load_images() -> tuple[list[ImageFile], list[int]]:
+    """"""
+    cars_imgs, cars_lbls = [], []
+    for i, subdir in enumerate(data_dir.glob('[!_]*')):
+        for img in subdir.iterdir():
+            img = Image.open(img)
+            cars_imgs.append(img)
+            cars_lbls.append(i)
+    return cars_imgs, cars_lbls
+
+
+def coerse_images(
+        cars_imgs: list[ImageFile],
+        cars_lbls: list[int],
         new_width: int | None = None,
         new_height: int | None = None,
         scale: bool = True,
 ) -> tuple[ndarray, ndarray]:
     """"""
     resize = new_width is not None and new_height is not None
-    cars_imgs, cars_lbls = [], []
-    for i, subdir in enumerate(data_dir.glob('[!_]*')):
-        for img in subdir.iterdir():
-            img = Image.open(img)
-            if resize:
-                img = img.resize((new_width, new_height), Resampling.BICUBIC)
-            cars_imgs.append(img)
-            cars_lbls.append(i)
+    for i in range(len(cars_imgs)):
+        img = cars_imgs[i]
+        if resize:
+            img = img.resize((new_width, new_height), Resampling.BICUBIC)
+        cars_imgs[i] = array(img)
     
-    cars_imgs = array([array(img) for img in cars_imgs])
+    cars_imgs = array(cars_imgs)
     cars_lbls = array(to_categorical(cars_lbls, num_classes=3))
     
     if scale:
@@ -45,6 +55,6 @@ if __name__ == '__main__':
     
     savez_compressed(
         script_dir / 'fmr.npz',
-        *load_images(),
+        *coerse_images(*load_images()),
     )
 
